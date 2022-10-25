4dc908f31ace9440d4675ee3776dc8a2c88b7b5a
George Cosma <george@whitecitycode.com>
2022-10-25 12:26:43 +0300

    added chakra

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../chakra/theme";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 }
