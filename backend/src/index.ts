b5e96837da4d3b45257127ec244a005a5182528d
George Cosma <george@whitecitycode.com>
2022-10-25 14:45:58 +0300

    added gql to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,37 @@
-console.log('hello world');
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-express';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import express from 'express';
+import http from 'http';
+import typeDefs from './graphql/typedefs';
+import resolvers from './graphql/resolvers';
+
+const startApolloServer = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+
+  console.log(`Server ready at http://localhost:400${server.graphqlPath}`);
+};
+
+startApolloServer().catch((err) => console.error(err));
