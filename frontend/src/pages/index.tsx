8a37b3e9a1636f3ab99c5e0e9078a4408feff0e5
George Cosma <george@whitecitycode.com>
2022-10-25 12:15:46 +0300

    setup + auth

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Home: NextPage = () => {
+  const { data } = useSession();
+
+  return (
+    <div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>Sign out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign in</button>
+      )}
+    </div>
+  );
+};
+
+export default Home;
